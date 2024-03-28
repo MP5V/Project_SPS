4f4641b8fb011c22f1cbefdb1f8bb0a40eaceae8
Ksenia <ksenia11122002@gmail.com>
2024-03-29 00:36:52 +0300

    Добавление функции GET_NOMEN с вытягиваем номенклатуры

--- Project_SPS/Develop.cs
+++ Project_SPS/Develop.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 using System.Web.Services.Description;
 using System.Windows.Forms;
 
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
 namespace Project_SPS
 {
     public partial class Develop : Form
@@ -66,6 +69,67 @@ namespace Project_SPS
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int Result;
+
+            string P1 = "GET_NOMEN";
+            string P2 = "";
+            string P3 = "";
+            string P4 = "";
+            string P5 = "";
+
+            dataSet1.NOMEN.Clear();
+
+            //Создание объекта для вызова веб-сервиса
+
+            WebReference.OBMEN web_1c = new WebReference.OBMEN();
+
+            System.Net.NetworkCredential nc = new System.Net.NetworkCredential();
+
+            nc.UserName = "WS_USER";
+            nc.Password = "123";
+
+            web_1c.PreAuthenticate = true;
+            web_1c.Credentials = nc;
+
+            try
+            {
+                Result = web_1c.Universal(ref P1, P2, P3, P4, P5);
+            }
+            catch (Exception ex)
+            {
+                Result = 0;
+                P1 = ex.Message;
+            }
+            if (Result == 0)
+            {
+                tb3_ServerResult.Text = Result.ToString();
+                textBox1.Text = "Ошибка " + P1;
+            }
+            else
+            {
+                tb3_ServerResult.Text = Result.ToString();
+                tb_JSON.Text = P1;
+
+                Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(P1);
+
+                foreach (NOMan nom in myDeserializedClass.NOMEN)
+                {
+                    DataRow new_row = dataSet1.NOMEN.NewRow();
+
+                    new_row["ID"] = nom.ID;
+                    new_row["NAME"] = nom.NAME;
+                    new_row["KOL"] = nom.KOL;
+                    new_row["START_EC"] = nom.START_EC;
+                    new_row["END_EC"] = nom.END_EC;
+                    new_row["RESPONS"] = nom.RESPONS;
+
+                    dataSet1.NOMEN.Rows.Add(new_row);
+                }
+            }
         }
     }
 }
